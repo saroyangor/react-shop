7ba687cb20dd1b382067b361132bb644dbf372d6
saroyangor <dr.saroyan98@gmail.com>
2022-05-15 16:50:56 +0400

    header and footer repo link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,9 @@ const Header = () => {
                 </a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li>
-                        <a href="#!">Repo</a>
+                        <a href="https://github.com/saroyangor/react-shop/tree/master">
+                            Repo
+                        </a>
                     </li>
                 </ul>
             </div>
